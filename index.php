36b6a0a15a66ea7e9c40871e7b74273ebefd5ed7
bhargav <bhargav.karanam@gmail.com>
2017-02-26 03:30:34 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,304 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>GDG Manipal</title>
+
+	<script type="text/javascript">
+		<!--
+//		if (screen.width <= 1200) {
+//		document.location = "http://m.gdgmanipal.in";
+//		}
+		//-->
+	</script>
+	<link rel="Shortcut icon" href="images/favicon.ico" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/gotham.css">
+	<!-- <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'> -->
+	<link rel="stylesheet" href="css/home.css">
+	<script type='text/javascript' src='js/jquery.js'></script>
+	<script type='text/javascript' src='js/jquery.stellar.js'></script>
+	<script type='text/javascript' src='js/home.js'></script>
+</head>
+<body>
+	<div class='website-container'>
+
+		<!-- LOADER OVERLAY -->
+
+		<div class='loader-overlay'>
+			<i class="fa fa-circle-o-notch fa-spin" id='loader'></i>
+		</div>
+
+		<div class='shadow'></div>
+		<div class='bg-page1' data-stellar-background-ratio="0.05"></div>
+		<div class='bg-page1-text'>
+			<img class='gdg-logo1' src="images/GDG.svg" alt="">
+			<div class='page1-mid-text'><span class='blue-text'>[</span><span class='white-text'>connect</span><span class='blue-text'>]</span></div>
+			<div ><i class="fa fa-chevron-down chevron1"></i></div>
+		</div>
+		<div class='text-page1'>
+			<div class='page-head' id='head1'>
+				What is GDG?
+			</div>
+			<div class='page-content'>
+				Google Developer Groups (GDGs) are for developers who are interested in Google’s developer technology; everything from the Android, Chrome, Drive, and Google Cloud platforms, to product APIs like the Cast API,Maps API, and YouTube API.
+
+					A GDG can take many forms — from just a few people getting together to watch our latest video, to large gatherings with demos and tech talks, to events like code sprints and hackathons.
+			</div>
+		</div>
+		<!-- SECOND PARALLAX IMAGE -->
+		<div class='bg-page2' data-stellar-background-ratio="0.1"></div>
+		<div class='bg-page2-text'>
+			<div class='bg-page2-mid-text'><span class='blue-text'>[</span><span class='white-text'>create</span><span class='blue-text'>]</span></div>
+		</div>
+		<div class='text-page2'>
+			<div class='page-head' id='head2'>Vision</div>
+			<div class='page-content'>
+				
+					We encourage and invite Technology Enthusiasts/ Developers/Designers /Interested Students/Professionals to be a part of this awesome community of like minded personalities and come up with innovative and futuristic product ideas, that is set to re-define the digital culture and people’s lives.
+			</div>
+		</div>
+		<div class='bg-page3' data-stellar-background-ratio="0.1"></div>
+		<div class='bg-page3-text'>
+			<div class='bg-page3-mid-text'><span class='blue-text'>[</span><span class='white-text'>collaborate</span><span class='blue-text'>]</span></div>
+		</div>
+		<div class='text-page3'>
+			<div class='page-head' id='wtm-head'>#WomenTechmakers</div>
+			<div class='page-content' id='wtm-content'>
+				
+				Google's Women Techmakers program provides visibility, community, and resources for women in technology.
+				<br>
+				<!-- Subscribe to GDG to get notifications from GDG. -->
+			</div>
+			<!-- <button id='wtm-button'> Join the community</button> -->
+			<!-- <div class='subscribe-container'>
+				<input type="text" id='subscribe-input'>
+				<span id='subscribe-btn'>Subscribe</span>	
+			</div>
+			<br>
+			<div id='subscribe-thankyou'>Thank you.</div> -->
+		</div>
+		<div class="separator"></div>
+		<iframe id="wtm-video" width="560" height="315" src="https://www.youtube.com/embed/tjFeLln_tfg" frameborder="0" allowfullscreen></iframe>
+		<div class="separator"></div>
+		<!-- PAST EVENTS -->
+		<!-- EVENT CARDS -->
+		<div class='past-events'>
+			<div class='events-box'>
+				<!-- EVENT STARTS -->
+				<div class='event-card'>
+					<div class='event-picture-box'>
+						<img src="images/node-poster.jpg" class='event-picture' alt="">
+					</div>
+					<div class='event-info'>
+						<div class='event-info-title'>Node.Js Workshop</div>
+						<div class='event-info-descrip'>
+							A workshop on Node.Js which introduced people to the MEAN stack.
+						</div>
+					</div>
+				</div>
+				<!-- EVENT STARTS -->
+				<div class='event-card'>
+					<div class='event-picture-box'>
+						<img src="images/web-workshop.jpg" class='event-picture' alt="">
+					</div>
+					<div class='event-info'>
+						<div class='event-info-title'>Web Development Workshop</div>
+						<div class='event-info-descrip'>
+							Two day workshop on HTML5 and JavaScript. Learning the basics of web development which
+
+serves as backbone for other Google Technologies. Google goodies were up for grabs.
+						</div>
+					</div>
+				</div>
+				<!-- EVENT STARTS -->
+				<div class='event-card'>
+					<div class='event-picture-box'>
+						<img src="images/wtm-event.jpg" class='event-picture' alt="">
+					</div>
+					<div class='event-info'>
+						<div class='event-info-title'>Google Cloud Workshop and Women TechMakers’ Introduction</div>
+						<div class='event-info-descrip'>
+							Introduction to Google Cloud as a platform to host mobile apps and using App Engine for
+
+backend. Also had a one hour introduction of GDG Women Community.
+						</div>
+					</div>
+				</div>
+				<!-- EVENT STARTS -->
+				<div class='event-card'>
+					<div class='event-picture-box'>
+						<img src="images/polymer-event.jpg" class='event-picture' alt="">
+					</div>
+					<div class='event-info'>
+						<div class='event-info-title'>Polymer Hackathon</div>
+						<div class='event-info-descrip'>
+							Held session on Polymer, the web framework by Google and submission for web apps.
+
+The participants submitted 11 web apps in total and were adjudged on the following criteria-
+
+Best UI, Best Idea and Best App.
+						</div>
+					</div>
+				</div>
+				<!-- EVENT STARTS -->
+				<div class='event-card'>
+					<div class='event-picture-box'>
+						<img src="images/android-wear.jpg" class='event-picture' alt="">
+					</div>
+					<div class='event-info'>
+						<div class='event-info-title'>Android Wear Hackathon</div>
+						<div class='event-info-descrip'>
+							The Hackathon was divided in two parts Android Hack and Wear Hack, with main focus on
+
+wear. The app idea could belong to any of the app categories available on the play store.
+
+Brownie points awarded to apps focusing on Education, Health, Information Access, Women 
+
+Security, Disaster Management.
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='events-fixed'>
+				<div>
+					<div class='page-head' id='events-page-title'>Our Initiatives</div>
+					<div class='events-page-text'>From 2014 to present, Women Techmakers is continually launching global scalable initiatives and piloting new programs to support and empower women in the industry. Receive updates from us when we're rolling out new initiatives.</div>
+				</div>	
+			</div>
+		</div>
+		<div class='events-fixed-sticky'>
+			<div class='page-head' id='events-page-title'>Our Initiatives</div>
+			<div class='events-page-text'>From 2014 to present, Women Techmakers is continually launching global scalable initiatives and piloting new programs to support and empower women in the industry. Receive updates from us when we're rolling out new initiatives.</div>
+		</div>	
+		
+		<div class='team-head'>Team</div>
+		<div class='team-page'>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/wally.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Aditya Walvekar</div>
+					<div class='mtenure'>2016 - Present</div>
+					<div class='mmail'>aditya@iecsemanipal.com</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/sukriti.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Sukriti Paul</div>
+					<div class='mtenure'>2016 - Present</div>
+					<div class='mmail'>sukriti@iecsemanipal.com</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/siddy.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Siddharth Kothiyal</div>
+					<div class='mtenure'>2016 - Present</div>
+					<div class='mmail'>siddharth@gdgmanipal.in</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/nari.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Arjun Narayan</div>
+					<div class='mtenure'>2016 - Present</div>
+					<div class='mmail'>arjun@iecsemanipal.com</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/bhargav.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Bhargav Karanam</div>
+					<div class='mtenure'>2016 - Present</div>
+					<div class='mmail'>bhargav@iecsemanipal.com</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/yash.png" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Yash Choukse</div>
+					<div class='mtenure'>2015 - 2016</div>
+					<div class='mmail'>yash@iecsemanipal.com</div>
+				</div>
+			</div>
+
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/muskan.png" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Muskan Gupta</div>
+					<div class='mtenure'>2015 - 2016</div>
+					<div class='mmail'>muskan.gupta@gmail.com</div>
+				</div>
+			</div>
+
+	
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/mehul.png" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Mehul Smriti Raje</div>
+					<div class='mtenure'>2014 - 2015</div>
+					<div class='mmail'>msr@iecsemanipal.com</div>
+				</div>
+			</div>
+
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/siddhant.png" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Siddhant Sinha</div>
+					<div class='mtenure'>2014 - 2015</div>
+					<div class='mmail'>siddhanth@iecsemanipal.com</div>
+				</div>
+			</div>
+			<div class='manager-block'>
+				<div class='mblock-1'>
+					<img class='mimg' src="images/rohit.jpg" alt="">
+				</div>
+				<div class='mblock-2'>
+					<div class='mname'>Rohit Nigam</div>
+					<div class='mtenure'>2013 - 2014</div>
+					<div class='mmail'>rohit@iecsemanipal.com</div>
+				</div>
+			</div>
+
+
+			<div class='link-row'>
+			<a href="https://www.facebook.com/GDGManipal?fref=ts"><i class="fa fa-facebook social-icon fb"></i></a>
+			<a href="https://developers.google.com/groups/chapter/101309722229928080208/"><i class="fa fa-google-plus-square social-icon gplus"></i></a>
+			<br> <br>
+			<div class='disclaimer'><b>Disclaimer:</b> GDG Manipal is an independent group; our activities and the opinions expressed on this website should in no way be linked to Google, the corporation.</div>
+		</div>
+
+
+		</div>
+
+		
+
+		<!-- FOOTER -->
+		<!-- <div class='foot' id='foot'>
+			developed by yash choukse / !zeko
+		</div> -->
+	</div>
+</body>
+</html>
---
+++ js/home.js
@@ -0,0 +1,70 @@
+
+
+var fixmeTop;
+var footTop;
+var pastEventsHeight;
+var winHeight;
+
+$(document).ready(function(){
+	//TO START THE STELLAR FRAMEWORK
+	$(window).stellar();
+	fixmeTop = $('.events-fixed').offset().top;       // get initial position of the element
+    footTop = $('.team-head').offset().top;
+    pastEventsHeight = $('.past-events').offset().top;
+    winHeight = $(window).height();
+    console.log(winHeight);
+    //console.log(pastEventsHeight);
+});
+
+
+// function easeOut(){
+//     $('.events-fixed-sticky').animate({
+//         opacity:0
+//     },300);
+// }
+
+
+$(window).scroll(function() {                  // assign scroll event listener
+    var currentScroll = $(window).scrollTop(); // get current position    
+
+    var tmp = footTop - winHeight;
+
+    if (currentScroll > tmp || currentScroll < fixmeTop)
+    {
+        
+        $('.events-fixed-sticky').css({                      // scroll to that element or below it
+            'display': 'none'
+        });
+    }
+    else if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.events-fixed-sticky').css({                      // scroll to that element or below it
+            'display': 'block'
+        });
+    }
+
+});
+
+$(document).on('click','#wtm-button',function(){
+    location.href = "https://www.facebook.com/Women-Techmakers-1602378576670289/";
+});
+
+$(document).on('click','#subscribe-btn',function(){
+    // get email id from form
+    var data = {};
+    data.email = $('#subscribe-input').val();
+    var url = "http://www.iecse.xyz/api/subscribe-gdg";
+    $.post(url,data,function(e){
+        $('#subscribe-thankyou').css({
+            'color':'#212121'
+        });
+    });
+
+});
+$(window).load(function(e){
+    $('#loader').css({
+       'display':'none'
+    });
+    $('.loader-overlay').animate({
+        'opacity':'0'
+    },2500);
+});
